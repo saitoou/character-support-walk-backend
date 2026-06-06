46e6e283e6d65bf63098c6bda6f6fa13f9c5cefb
kohei <kouhei02.3110@gmail.com>
2026-06-06 18:05:08 +0900

    defer実行の修正

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,19 +16,26 @@ import (
 	"github.com/chocoko/character-support-walk-backend/internal/logger"
 	appmiddlware "github.com/chocoko/character-support-walk-backend/internal/middleware"
 	"github.com/chocoko/character-support-walk-backend/internal/router"
+	echoprometheus "github.com/labstack/echo-contrib/v5/echoprometheus"
 	echootel "github.com/labstack/echo-opentelemetry"
 	"github.com/labstack/echo/v5"
 	echomiddleware "github.com/labstack/echo/v5/middleware"
 )
 
 func main() {
+	if err := run(); err != nil {
+		slog.Error("application error", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 
 	ctx := context.Background()
 
 	shutdown, err := observability.InitTracer(ctx)
 	if err != nil {
-		slog.Error("failed to init tracer", "error", err)
-		os.Exit(1)
+		return err
 	}
 	defer func() {
 		if err := shutdown(ctx); err != nil {
@@ -42,15 +49,13 @@ func main() {
 
 	db, err := database.NewDB(cfg)
 	if err != nil {
-		slog.Error("failed to setup database", "error", err)
-		os.Exit(1)
+		return err
 	}
 	defer db.Close()
 
 	ttl, err := time.ParseDuration(cfg.Auth.JWTAccessTokenTTL)
 	if err != nil {
-		slog.Error("failed to convert access token ttl")
-		os.Exit(1)
+		return err
 	}
 
 	jwtService := auth.NewJWTService(cfg.Auth.JWTSecret, ttl, cfg.Auth.JWTIssuer)
@@ -58,10 +63,13 @@ func main() {
 	e := echo.New()
 	e.Use(echomiddleware.Recover())
 	e.Use(echootel.NewMiddleware("character-support-walk-api"))
+	e.Use(echoprometheus.NewMiddleware("character_support_walk"))
 	e.Use(appmiddlware.CustomCORS())
 	e.Use(appmiddlware.RateLimiter())
 	e.Use(echomiddleware.RequestID())
 	e.Use(appmiddlware.RequestLogger())
+
+	e.GET("/metrics", echoprometheus.NewHandler())
 
 	diContainer := container.New(db, jwtService, cfg)
 	router.RegisterRoutes(e, diContainer)
@@ -76,9 +84,9 @@ func main() {
 	slog.Info("server started", "port", 8080)
 
 	if err := sc.Start(ctx, e); err != nil {
-		slog.Error("failed to start server", "error", err)
-		os.Exit(1)
+		return err
 	}
 
 	slog.Info("server shutdown completed")
+	return nil
 }
