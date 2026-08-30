43942c44656acbe055c212ecaf0c52980d99b4c7
kohei <kouhei02.3110@gmail.com>
2026-08-30 13:47:15 +0900

    linter

--- internal/middleware/rate_limit.go
+++ internal/middleware/rate_limit.go
@@ -9,7 +9,7 @@ import (
 
 func RateLimiter() echo.MiddlewareFunc {
 	return middleware.RateLimiterWithConfig(middleware.RateLimiterConfig{
-		Store: middleware.NewRateLimiterMemoryStore(100),
+		Store: middleware.NewRateLimiterMemoryStore(1000),
 		ErrorHandler: func(c *echo.Context, err error) error {
 			return c.JSON(http.StatusTooManyRequests, map[string]string{
 				"message": "too many request",
